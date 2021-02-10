f3c658cbd73e2184f3a6fbe20e7a090c4b5ac0f7
nakaokagabriel <gabrielnakaoka_99@hotmail.com>
2021-02-09 22:30:22 -0300

    add scroll in form mobile and title

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -11,6 +11,7 @@ import Button from '../../components/Button';
 
 import cardAcessIcon from '../../assets/icons/card.svg';
 import lockIcon from '../../assets/icons/lock.svg';
+import artBg from '../../assets/login-art.svg';
 
 const Login = () => {
   return (
@@ -20,21 +21,24 @@ const Login = () => {
           <div className="bg-login">
             <h1>Let`s sign you in</h1>
             <p>Welcome back.</p>
+            <img src={artBg} alt="art" />
           </div>
         </Col>
         <Col column={3}>
           <Form>
-            <Input icon={cardAcessIcon} text="Username/Email" />
-            <Input icon={lockIcon} text="Password" />
-            <Button text="Sign In" />
-            <div>
-              <Link to="/">Forgot Password</Link>
-            </div>
-            <p>
-              Not a member?
-              {' '}
-              <Link to="/">Sign Up</Link>
-            </p>
+            <form>
+              <Input icon={cardAcessIcon} text="Username/Email" />
+              <Input icon={lockIcon} text="Password" />
+              <Button text="Sign In" />
+              <div>
+                <Link to="/">Forgot Password</Link>
+              </div>
+              <p>
+                Not a member?
+                {' '}
+                <Link to="/">Sign Up</Link>
+              </p>
+            </form>
           </Form>
         </Col>
       </Row>
--- src/pages/Login/styles.ts
+++ src/pages/Login/styles.ts
@@ -2,5 +2,34 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
   background: #2DCDA3;
-  height: 100vh;
+
+  .bg-login {
+    position: relative;
+    z-index: 10;
+    padding: 0 20px;
+    height: 50vh;
+
+    h1 {
+      padding: 40px 0 10px;
+      color: #fff;
+    }
+
+    p {
+      color: #F3F4F8;
+      font-weight: bold;
+    }
+
+    img {
+      width: 230px;
+      position: absolute;
+      top: 10%;
+      left: 50%;
+      transform: translate3d(-50%, 0, 0);
+      z-index: -1;
+
+      @media screen and (min-width: 420px) {
+        width: 250px;
+      }
+    }
+  }
 `;
