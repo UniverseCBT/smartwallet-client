c260204a41843018cb92c9907cf814b5c74ca805
nakaokagabriel <gabrielnakaoka_99@hotmail.com>
2021-02-21 01:40:23 -0300

    create starts sctructure

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import Login from '../pages/Login';
+import Register from '../pages/Register';
 
 const Routes = () => {
   return (
     <BrowserRouter>
       <Switch>
         <Route path="/login" component={Login} />
+        <Route path="/register" component={Register} />
       </Switch>
     </BrowserRouter>
   )
