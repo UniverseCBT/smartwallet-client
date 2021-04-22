56c83fa19310e5818e006db08371a001c221e80a
nakaokagabriel <gabrielnakaoka_99@hotmail.com>
2021-04-21 23:44:02 -0300

    styles list actions

--- src/components/Register/List/index.tsx
+++ src/components/Register/List/index.tsx
@@ -1,8 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-import { Container, Description, Utils, Features, Actions } from './styles';
+import {
+  Container,
+  Itens,
+  Description,
+  Utils,
+  WrapperUtils,
+  Features,
+  Select,
+  Actions
+} from './styles';
 
-import moreIcon from '../../../assets/icons/more.svg';
+import { ReactComponent as Menu } from '../../../assets/icons/more.svg';
+import { ReactComponent as Close } from '../../../assets/icons/close.svg';
+import { ReactComponent as Pen } from '../../../assets/icons/pen.svg';
+import { ReactComponent as Trash } from '../../../assets/icons/trash.svg';
+
+import { transformMoney } from '../../../utils/currency';
 
 type DataProps = {
   id: string;
@@ -17,33 +31,43 @@ type Props = {
 };
 
 const List = ({ data }: Props) => {
-  const teste = new Intl.NumberFormat('en-US', {
-    style: 'currency',
-    currency: 'USD'
-  });
+  const [showSelect, setShowSelect] = useState(false);
 
   return (
-    <>
+    <Container>
+      {data.length > 0 && (
+        <Actions showSelect={showSelect}>
+          <button type="button" onClick={() => setShowSelect(!showSelect)}>
+            Edit / Delete
+            <div>{showSelect ? <Close /> : <Menu />}</div>
+          </button>
+        </Actions>
+      )}
       {data.map(dataItem => (
-        <Container key={dataItem.id}>
+        <Itens key={dataItem.id}>
           <Description>
             <h6>{dataItem.title}</h6>
             <span>{dataItem.description}</span>
           </Description>
           <Utils>
-            <Features>
-              <strong>{teste.format(dataItem.money)}</strong>
-              {dataItem.category && <span>{dataItem.category}</span>}
-            </Features>
-            <Actions>
-              <button type="button">
-                <img src={moreIcon} alt="three points" />
-              </button>
-            </Actions>
+            <WrapperUtils>
+              <Features showSelect={showSelect}>
+                <strong>{transformMoney(dataItem.money)}</strong>
+                {dataItem.category && <span>{dataItem.category}</span>}
+              </Features>
+              <Select showSelect={showSelect}>
+                <button type="button">
+                  <Pen width={15} height={15} />
+                </button>
+                <button type="button">
+                  <Trash width={15} height={15} />
+                </button>
+              </Select>
+            </WrapperUtils>
           </Utils>
-        </Container>
+        </Itens>
       ))}
-    </>
+    </Container>
   );
 };
 
