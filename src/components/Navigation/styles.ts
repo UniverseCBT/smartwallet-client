741b53d4d9c01d23916bcedab000428d8294af13
nakaokagabriel <gabrielnakaoka_99@hotmail.com>
2021-05-04 22:15:10 -0300

    modify navigation mobile

--- src/components/Navigation/styles.ts
+++ src/components/Navigation/styles.ts
@@ -3,18 +3,37 @@ import { Link } from 'react-router-dom';
 
 export const NavigationWrapper = styled.nav`
   position: relative;
-  width: auto;
   height: 100vh;
+  width: 55px;
   overflow-y: hidden;
   background-color: #fff;
 `;
 
 export const NavigationItems = styled.div`
   display: flex;
-  flex-direction: column;
   align-items: center;
+  flex-direction: column;
+  padding: 20px 15px;
 `;
 
-export const ItemsContent = styled.div``;
+export const ItemsContent = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  width: 100%;
 
-export const Content = styled(Link)``;
+  border-top: 1px solid #f3f4f8;
+  margin-top: 20px;
+  padding-top: 20px;
+`;
+
+export const Content = styled(Link)`
+  & + a {
+    margin-top: 15px;
+  }
+
+  img {
+    width: 30px;
+    height: 26px;
+  }
+`;
