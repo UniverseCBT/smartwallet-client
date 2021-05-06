b65c03a9d40acd0511363f19c64bb2575ea2f8bc
nakaokagabriel <gabrielnakaoka_99@hotmail.com>
2021-05-05 22:44:17 -0300

    all screens styles

--- src/components/Navigation/index.tsx
+++ src/components/Navigation/index.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import {
   NavigationWrapper,
   NavigationItems,
+  NavigationContent,
   ItemsContent,
   Content
 } from './styles';
@@ -11,20 +12,28 @@ import Logo from '../icons/Logo';
 
 import dashboardIcon from '../../assets/icons/dashboard.svg';
 import historicIcon from '../../assets/icons/historic.svg';
+import exitIcon from '../../assets/icons/exit.svg';
 
 const Navigation = () => {
   return (
     <NavigationWrapper>
       <NavigationItems>
         <Logo />
-        <ItemsContent>
-          <Content to="/">
-            <img src={dashboardIcon} alt="floater" />
-          </Content>
-          <Content to="/historic">
-            <img src={historicIcon} alt="crusades arrow" />
-          </Content>
-        </ItemsContent>
+        <NavigationContent>
+          <ItemsContent id="center">
+            <Content to="/">
+              <img src={dashboardIcon} alt="floater" />
+            </Content>
+            <Content to="/historic">
+              <img src={historicIcon} alt="crusades arrow" />
+            </Content>
+          </ItemsContent>
+          <ItemsContent id="end">
+            <Content to="/historic">
+              <img src={exitIcon} alt="door with arrow" />
+            </Content>
+          </ItemsContent>
+        </NavigationContent>
       </NavigationItems>
     </NavigationWrapper>
   );
--- src/components/Navigation/styles.ts
+++ src/components/Navigation/styles.ts
@@ -2,34 +2,76 @@ import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 
 export const NavigationWrapper = styled.nav`
-  position: relative;
-  height: 100vh;
-  width: 55px;
-  overflow-y: hidden;
   background-color: #fff;
+  width: 100%;
+  padding: 15px 10px;
+
+  @media screen and (min-width: 575px) {
+    padding: 0;
+    position: relative;
+    height: 100vh;
+    width: 55px;
+    overflow-y: hidden;
+  }
+
+  @media screen and (min-width: 1024px) {
+    width: 90px;
+  }
 `;
 
 export const NavigationItems = styled.div`
   display: flex;
+  justify-content: space-between;
   align-items: center;
-  flex-direction: column;
-  padding: 20px 15px;
+  width: 100%;
+
+  @media screen and (min-width: 575px) {
+    align-items: center;
+    flex-direction: column;
+    padding: 20px 15px;
+    height: 100%;
+  }
+`;
+
+export const NavigationContent = styled.div`
+  width: 60%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  @media screen and (min-width: 575px) {
+    width: auto;
+    flex-direction: column;
+    height: 100%;
+  }
 `;
 
 export const ItemsContent = styled.div`
-  display: flex;
-  align-items: center;
-  flex-direction: column;
-  width: 100%;
+  @media screen and (min-width: 575px) {
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    width: 100%;
 
-  border-top: 1px solid #f3f4f8;
-  margin-top: 20px;
-  padding-top: 20px;
+    border-top: 1px solid #f3f4f8;
+    margin-top: 20px;
+    padding-top: 20px;
+
+    &#end {
+      border-top: none;
+      align-self: flex-end;
+    }
+  }
 `;
 
 export const Content = styled(Link)`
   & + a {
-    margin-top: 15px;
+    margin-left: 5px;
+
+    @media screen and (min-width: 575px) {
+      margin-top: 15px;
+      margin-left: 0;
+    }
   }
 
   img {
