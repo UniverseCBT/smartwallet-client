42d4697eba1b6f31169a0ae80c3df5af034bea3d
Gabriel Nakaoka <gabrielnakaoka_99@hotmail.com>
2021-11-30 20:46:07 -0300

    fature: start create toast

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
-import { Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import { Router } from 'react-router-dom';
 
-import { history } from 'services/history';
 import store from 'store';
 
-import GlobalStyle from 'styles/global';
+import { history } from 'services/history';
+
 import Route from './routes';
+
+import GlobalStyle from 'styles/global';
 
 function App() {
   return (
---
+++ src/components/Toast/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import * as S from './styles';
+
+const Toast = () => {
+  return (
+    <S.Container>
+      <h1>hello</h1>
+    </S.Container>
+  );
+};
+
+export default Toast;
